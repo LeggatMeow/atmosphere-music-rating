bb9534c8d0c06ae50ed9e938b6084005752a34f3
LeggatMeow <LeggatMeow@gmail.com>
2025-10-25 18:08:14 +0100

    Update bandData.js
    Refrencing tracks to bring them in

--- src/data/bandData.js
+++ src/data/bandData.js
@@ -1,24 +1,40 @@
 // src/data/bandData.js
 
+import {
+  overcastTracks,
+  godLovesUglyTracks,
+  sevensTravelsTracks,
+  youCantImagineTracks,
+  whenLifeGivesYouLemonsTracks,
+  theFamilySignTracks,
+  southsidersTracks,
+  fishingBluesTracks,
+  miVidaLocalTracks,
+  wheneverTracks,
+  theDayBeforeHalloweenTracks,
+  wordTracks,
+  soManyOtherRealitiesTracks,
+  jesturesTracks
+} from './tracks';
+
 export const bandData = {
   name: "Atmosphere",
   description: "Atmosphere is an American hip hop duo consisting of rapper Slug (Sean Daley) and DJ/producer Ant (Anthony Davis).",
   albums: [
-    // Studio Albums
-    { id: "overcast", title: "Overcast!", year: 1997, cover: null },
-    { id: "god-loves-ugly", title: "God Loves Ugly", year: 2002, cover: null },
-    { id: "sevens-travels", title: "Seven's Travels", year: 2003, cover: null },
-    { id: "you-cant-imagine-how-much-fun-were-having", title: "You Can't Imagine How Much Fun We're Having", year: 2005, cover: null },
-    { id: "when-life-gives-you-lemons", title: "When Life Gives You Lemons, You Paint That Shit Gold", year: 2008, cover: null },
-    { id: "the-family-sign", title: "The Family Sign", year: 2011, cover: null },
-    { id: "southsiders", title: "Southsiders", year: 2014, cover: null },
-    { id: "fishing-blues", title: "Fishing Blues", year: 2016, cover: null },
-    { id: "mi-vida-local", title: "Mi Vida Local", year: 2018, cover: null },
-    { id: "whenever", title: "Whenever", year: 2019, cover: null },
-    { id: "the-day-before-halloween", title: "The Day Before Halloween", year: 2020, cover: null },
-    { id: "word", title: "Word?", year: 2021, cover: null },
-    { id: "so-many-other-realities-exist-simultaneously", title: "So Many Other Realities Exist Simultaneously", year: 2023, cover: null },
-    { id: "jestures", title: "Jestures", year: 2025, cover: null },
+    { ...overcastTracks, cover: null },
+    { ...godLovesUglyTracks, cover: null },
+    { ...sevensTravelsTracks, cover: null },
+    { ...youCantImagineTracks, cover: null },
+    { ...whenLifeGivesYouLemonsTracks, cover: null },
+    { ...theFamilySignTracks, cover: null },
+    { ...southsidersTracks, cover: null },
+    { ...fishingBluesTracks, cover: null },
+    { ...miVidaLocalTracks, cover: null },
+    { ...wheneverTracks, cover: null },
+    { ...theDayBeforeHalloweenTracks, cover: null },
+    { ...wordTracks, cover: null },
+    { ...soManyOtherRealitiesTracks, cover: null },
+    { ...jesturesTracks, cover: null },
 
     // EPs
     { id: "ford-one", title: "Ford One", year: 2000, cover: null },
